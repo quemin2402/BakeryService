bcb43baae433ac58a863831eeede1efb006ffc2e
quemin2402 <mereyibraim2006@gmail.com>
2024-12-18 14:09:29 +0500

    Add database connection and migration setup in db folder

---
+++ db/connection.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"BakeryService/models"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	connStr := "host=localhost user=postgres dbname=bakery port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	err = db.AutoMigrate(&models.Product{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	log.Println("Connected to the database successfully!")
+	return db
+}
