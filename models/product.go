4855b6b733b600cfab0c7871c7576db8df72a9c2
quemin2402 <mereyibraim2006@gmail.com>
2024-12-18 14:18:48 +0500

    Add product model for database schema

---
+++ models/product.go
@@ -0,0 +1,9 @@
+package models
+
+type Product struct {
+	ID          uint    `json:"id" gorm:"primaryKey"`
+	Name        string  `json:"name"`
+	Price       float64 `json:"price"`
+	Description string  `json:"description"`
+	Image       string  `json:"image"`
+}
