1355bcb3fdc9e76234f6ddfa04276b6b478c7781
quemin2402 <mereyibraim2006@gmail.com>
2025-01-08 04:02:54 +0500

    Fully done: 3. Filtering, Sorting, and Pagination

--- main.go
+++ main.go
@@ -19,11 +19,11 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
-	r.HandleFunc("/api/products", handlers.GetAllProducts(database)).Methods("GET")
 	r.HandleFunc("/api/product/{id}", handlers.GetProductByID(database)).Methods("GET")
 	r.HandleFunc("/api/product/create", handlers.CreateProduct(database)).Methods("POST")
 	r.HandleFunc("/api/product/update", handlers.UpdateProduct(database)).Methods("PUT")
 	r.HandleFunc("/api/product/delete/{id}", handlers.DeleteProduct(database)).Methods("DELETE")
+	r.HandleFunc("/api/products", handlers.GetFilteredProducts(database)).Methods("GET")
 
 	fs := http.FileServer(http.Dir("./static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
