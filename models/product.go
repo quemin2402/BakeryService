d30e117a67501b99c22c22c250a7e23820c506e0
quemin2402 <mereyibraim2006@gmail.com>
2025-01-08 04:01:06 +0500

    Category column in the database

--- models/product.go
+++ models/product.go
@@ -5,5 +5,6 @@ type Product struct {
 	Name        string  `json:"name"`
 	Price       float64 `json:"price"`
 	Description string  `json:"description"`
+	Category    string  `json:"category"`
 	Image       string  `json:"image"`
 }
